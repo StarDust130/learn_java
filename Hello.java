2bc1ecd042501ac0dbb78d908761dcbcaee2c2ca
StarDust130 <csyadav0513@gmail.com>
2026-03-31 20:12:35 +0530

    add additional classes Munna and Okay with main methods

--- Hello.java
+++ Hello.java
@@ -3,3 +3,17 @@ class Hello {
         System.out.println("Hello, World!");
     }
 }
+
+
+class Munna{
+    public static void main(String[] args){
+        System.out.println("Munna is Piggy 🐷");
+    }
+}
+
+
+class Okay{
+    public static void main(String[] args){
+        System.out.println("IS This Okay! 🫣");
+    }
+}
